9d042b09a56d6096bdb84bd9916508396e4f0044
kry-dev <yaroslavkryvda@gmail.com>
2017-11-22 18:08:48 +0200

    start template

---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+//= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+
+
+
+
+
+//= commons/tooltip.js
+//= commons/dropdown.js
